9e56cc0a167b0d41ce1afa0d47e1d8de904e35a3
Ken Woodworth <ken@atendesigngroup.com>
2014-06-24 14:27:06 -0400

    updates structure and class names

--- index.php
+++ index.php
@@ -16,40 +16,79 @@
       <h2 class="sg-section-title">Colors</h2>
       <article class="sg-item">
         <header class="sg-item-header">
-          <h3 class="sg-item-title">Primary</h3>
+          <h3 class="sg-item-title">Color Palettes</h3>
         </header>
-        <ul class="sg-palette">
-          <li class="sg-color" style="background:#e5efeb;"><strong>$teal-white</strong><span>#e5efeb</span></li>
-          <li class="sg-color" style="background:#dae8e2;"><strong>$teal-lightest</strong><span>#dae8e2</span></li>
-          <li class="sg-color" style="background:#3dd59e;"><strong>$teal-lighter</strong><span>#3dd59e</span></li>
-          <li class="sg-color" style="background:#00c584;"><strong>$teal-light</strong><span>#00c584</span></li>
-          <li class="sg-color sg-color-main" style="background:#01986c;"><strong>$teal</strong><span>#01986c</span></li>
-          <li class="sg-color" style="background:#017864;"><strong>$teal-dark</strong><span>#017864</span></li>
-          <li class="sg-color" style="background:#00585c;"><strong>$teal-darker</strong><span>#00585c</span></li>
-          <li class="sg-color" style="background:#001d20;"><strong>$teal-darkest</strong><span>#001d20</span></li>
-        </ul>
-      </article>
-      <article class="sg-item">
-        <header class="sg-item-header">
-          <h3 class="sg-item-title">Secondary</h3>
-        </header>
-        <ul class="sg-palette">
-          <li class="sg-color sg-color-main" style="background:#50625c;"><strong>$gray-green</strong><span>#50625c</span></li>
-        </ul>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Primary</h4>
+          <ul class="sg-palette">
+            <li class="sg-color" style="background:#e5e5e5;"><strong>$gray-white</strong><span>#e5e5e5</span></li>
+            <li class="sg-color" style="background:#cccccc;"><strong>$gray-lightest</strong><span>#cccccc</span></li>
+            <li class="sg-color" style="background:#b2b2b2;"><strong>$gray-lighter</strong><span>#b2b2b2</span></li>
+            <li class="sg-color" style="background:#999999;"><strong>$gray-light</strong><span>#999999</span></li>
+            <li class="sg-color sg-color-main" style="background:#727272;"><strong>$gray</strong><span>#727272</span></li>
+            <li class="sg-color" style="background:#666666;"><strong>$gray-dark</strong><span>#666666</span></li>
+            <li class="sg-color" style="background:#595959;"><strong>$gray-darker</strong><span>#595959</span></li>
+            <li class="sg-color" style="background:#333333;"><strong>$gray-darkest</strong><span>#333333</span></li>
+            <li class="sg-color" style="background:#222222;"><strong>$gray-black</strong><span>#222222</span></li>
+          </ul>
+        </div>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Secondary</h4>
+          <ul class="sg-palette">
+            <li class="sg-color" style="background:#e5e5e5;"><strong>$gray-white</strong><span>#e5e5e5</span></li>
+            <li class="sg-color" style="background:#cccccc;"><strong>$gray-lightest</strong><span>#cccccc</span></li>
+            <li class="sg-color sg-color-main" style="background:#b2b2b2;"><strong>$gray-lighter</strong><span>#b2b2b2</span></li>
+            <li class="sg-color" style="background:#999999;"><strong>$gray-light</strong><span>#999999</span></li>
+            <li class="sg-color" style="background:#727272;"><strong>$gray</strong><span>#727272</span></li>
+            <li class="sg-color" style="background:#666666;"><strong>$gray-dark</strong><span>#666666</span></li>
+            <li class="sg-color sg-color-main" style="background:#595959;"><strong>$gray-darker</strong><span>#595959</span></li>
+            <li class="sg-color sg-color-main" style="background:#333333;"><strong>$gray-darkest</strong><span>#333333</span></li>
+            <li class="sg-color" style="background:#222222;"><strong>$gray-black</strong><span>#222222</span></li>
+          </ul>
+        </div>
       </article>
     </section>
     <section class="sg-section">
       <h2 class="sg-section-title">Typography</h2>
       <article class="sg-item">
         <header class="sg-item-header">
+          <h3 class="sg-item-title">Fonts</h3>
+        </header>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Sans</h4>
+          <h5 class="sg-font-title ff-sans">Arial</h5>
+          <p class="sg-font-style ff-sans">Regular</p>
+          <p class="sg-font-style ff-sans"><em>Italic</em></p>
+          <p class="sg-font-style ff-sans"><strong>Bold</strong></p>
+          <p class="sg-font-style ff-sans"><strong><em>Bold Italic</em></strong></p>
+        </div>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Serif</h4>
+          <h5 class="sg-font-title ff-serif">Georgia</h5>
+          <p class="sg-font-style ff-serif">Regular</p>
+          <p class="sg-font-style ff-serif"><em>Italic</em></p>
+          <p class="sg-font-style ff-serif"><strong>Bold</strong></p>
+          <p class="sg-font-style ff-serif"><strong><em>Bold Italic</em></strong></p>
+        </div>
+      </article>
+      <article class="sg-item">
+        <header class="sg-item-header">
           <h3 class="sg-item-title">Headings</h3>
         </header>
-        <h1>Heading 1 with a <a href="#">link</a></h1>
-        <h2>Heading 2 with a <a href="#">link</a></h2>
-        <h3>Heading 3 with a <a href="#">link</a></h3>
-        <h4>Heading 4 with a <a href="#">link</a></h4>
-        <h5>Heading 5 with a <a href="#">link</a></h5>
-        <h6>Heading 6 with a <a href="#">link</a></h6>
+        <div class="sg-element">
+          <h4 class="sg-element-title">H1</h4>
+          <h1>Heading 1 with a <a href="#">link</a></h1>
+          <h4 class="sg-element-title">H2</h4>
+          <h2>Heading 2 with a <a href="#">link</a></h2>
+          <h4 class="sg-element-title">H3</h4>
+          <h3>Heading 3 with a <a href="#">link</a></h3>
+          <h4 class="sg-element-title">H4</h4>
+          <h4>Heading 4 with a <a href="#">link</a></h4>
+          <h4 class="sg-element-title">H5</h4>
+          <h5>Heading 5 with a <a href="#">link</a></h5>
+          <h4 class="sg-element-title">H6</h4>
+          <h6>Heading 6 with a <a href="#">link</a></h6>
+        </div>
       </article>
     </section>
   </div>
