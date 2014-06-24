3ae101afd0562cb039affd6d100986e701f6b8b5
Ken Woodworth <ken@atendesigngroup.com>
2014-06-24 16:49:34 -0400

    updates markup with even more elements

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
 <head>
   <title>StyleGuide</title>
-  <link rel="stylesheet" href="../css/screen.css">
+  <?php //  Remove this and add a path to your sites CSS file here ?>
   <link rel="stylesheet" href="css/styleguide.css">
 </head>
 
@@ -88,6 +88,413 @@
           <h5>Heading 5 with a <a href="#">link</a></h5>
           <h4 class="sg-element-title">H6</h4>
           <h6>Heading 6 with a <a href="#">link</a></h6>
+        </div>
+      </article>
+      <article class="sg-item">
+        <header class="sg-item-header">
+          <h3 class="sg-item-title">Body</h3>
+        </header>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Paragraph</h4>
+          <p>Nullam in dui mauris. Vivamus hendrerit arcu sed erat molestie vehicula. Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor. Suspendisse dictum feugiat nisl ut dapibus. Mauris iaculis porttitor posuere. Praesent id metus massa, ut blandit odio. Proin quis tortor orci.</p>
+        </div>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Blockquote</h4>
+          <blockquote>
+            <p>Nullam in dui mauris. Vivamus hendrerit arcu sed erat molestie vehicula. Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor. Suspendisse dictum feugiat nisl ut dapibus. Mauris iaculis porttitor posuere. Praesent id metus massa, ut blandit odio. Proin quis tortor orci.</p>
+          </blockquote>
+        </div>
+      </article>
+      <article class="sg-item">
+        <header class="sg-item-header">
+          <h3 class="sg-item-title">Lists</h3>
+        </header>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Ordered List</h4>
+          <ol>
+            <li>Nullam in dui mauris. Vivamus hendrerit arcu sed erat molestie vehicula.</li>
+            <li>Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor.</li>
+            <li>Ut in nulla enim.</li>
+            <li>Phasellus molestie magna non est bibendum non.</li>
+          </ol>
+        </div>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Unordered</h4>
+          <ul>
+            <li>Nullam in dui mauris. Vivamus hendrerit arcu sed erat molestie vehicula.</li>
+            <li>Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor.</li>
+            <li>Ut in nulla enim.</li>
+            <li>Phasellus molestie magna non est bibendum non.</li>
+          </ul>
+        </div>
+      </article>
+      <article class="sg-item">
+        <header class="sg-item-header">
+          <h3 class="sg-item-title">Meta</h3>
+        </header>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Byline</h4>
+          <p class="meta meta-byline">by <strong>Lisa Smith</strong></p>
+        </div>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Dateline</h4>
+          <p class="meta meta-dateline">December 12th, 2014</p>
+        </div>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Field / Value</h4>
+          <p><strong class="field-label">Label:</strong> <span class="field-value">Value</span></p>
+          <p><strong class="field-label">Label:</strong> <span class="field-value">Value 1</span>, <span class="field-value">Value 2</span>, <span class="field-value">Value 3</span></p>
+        </div>
+      </article>
+    </section>
+    <section class="sg-section">
+      <h2 class="sg-section-title">Images</h2>
+      <article class="sg-item">
+        <header class="sg-item-header">
+          <h3 class="sg-item-title">Image Sizes</h3>
+        </header>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Thumbnail</h4>
+          <img src="http://placehold.it/72x60/e5e5e5/999999">
+        </div>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Small</h4>
+          <img src="http://placehold.it/180x120/e5e5e5/999999">
+        </div>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Medium</h4>
+          <img src="http://placehold.it/240x180/e5e5e5/999999">
+        </div>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Large</h4>
+          <img src="http://placehold.it/420x300/e5e5e5/999999">
+        </div>
+      </article>
+      <article class="sg-item">
+        <header class="sg-item-header">
+          <h3 class="sg-item-title">Image Sizes</h3>
+        </header>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Image with Caption</h4>
+          <div class="image">
+            <img src="http://placehold.it/420x300/e5e5e5/999999">
+            <p class="caption">Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor. Suspendisse dictum feugiat nisl ut dapibus.</p>
+          </div>
+        </div>
+      </article>
+    </section>
+    <section class="sg-section">
+      <h2 class="sg-section-title">UI Elements</h2>
+      <article class="sg-item">
+        <header class="sg-item-header">
+          <h3 class="sg-item-title">Buttons</h3>
+        </header>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Primary</h4>
+          <div class="sg-element-column">
+            <h5 class="sg-element-column-title">Normal</h5>
+            <p><a href="#" class="button--primary">Read More</a></p>
+          </div>
+          <div class="sg-element-column">
+            <h5 class="sg-element-column-title">Hover</h5>
+            <p><a href="#" class="button--primary button--hover">Read More</a></p>
+          </div>
+          <div class="sg-element-column">
+            <h5 class="sg-element-column-title">Active</h5>
+            <p><a href="#" class="button--primary button--active">Read More</a></p>
+          </div>
+          <div class="sg-element-column">
+            <h5 class="sg-element-column-title">Disabled</h5>
+            <p><a href="#" class="button--primary button--disabled">Read More</a></p>
+          </div>
+        </div>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Secondary</h4>
+          <div class="sg-element-column">
+            <h5 class="sg-element-column-title">Normal</h5>
+            <p><a href="#" class="button--secondary">Read More</a></p>
+          </div>
+          <div class="sg-element-column">
+            <h5 class="sg-element-column-title">Hover</h5>
+            <p><a href="#" class="button--secondary button--hover">Read More</a></p>
+          </div>
+          <div class="sg-element-column">
+            <h5 class="sg-element-column-title">Active</h5>
+            <p><a href="#" class="button--secondary button--active">Read More</a></p>
+          </div>
+          <div class="sg-element-column">
+            <h5 class="sg-element-column-title">Disabled</h5>
+            <p><a href="#" class="button--secondary button--disabled">Read More</a></p>
+          </div>
+        </div>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Tertiary</h4>
+          <div class="sg-element-column">
+            <h5 class="sg-element-column-title">Normal</h5>
+            <p><a href="#" class="button--tertiary">Read More</a></p>
+          </div>
+          <div class="sg-element-column">
+            <h5 class="sg-element-column-title">Hover</h5>
+            <p><a href="#" class="button--tertiary button--hover">Read More</a></p>
+          </div>
+          <div class="sg-element-column">
+            <h5 class="sg-element-column-title">Active</h5>
+            <p><a href="#" class="button--tertiary button--active">Read More</a></p>
+          </div>
+          <div class="sg-element-column">
+            <h5 class="sg-element-column-title">Disabled</h5>
+            <p><a href="#" class="button--tertiary button--disabled">Read More</a></p>
+          </div>
+        </div>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Links</h4>
+          <div class="sg-element-column">
+            <h5 class="sg-element-column-title">Normal</h5>
+            <p>&#8230;Vivamus <a href="#">hendrerit</a> arcu&#8230;</p>
+          </div>
+          <div class="sg-element-column">
+            <h5 class="sg-element-column-title">Hover</h5>
+            <p>&#8230;Vivamus <a href="#" class="link--hover">hendrerit</a> arcu&#8230;</p>
+          </div>
+          <div class="sg-element-column">
+            <h5 class="sg-element-column-title">Active</h5>
+            <p>&#8230;Vivamus <a href="#" class="link--active">hendrerit</a> arcu&#8230;</p>
+          </div>
+        </div>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Read More</h4>
+          <div class="sg-element-column">
+            <h5 class="sg-element-column-title">Normal</h5>
+            <p><a href="#" class="link--more">Read More</a></p>
+          </div>
+          <div class="sg-element-column">
+            <h5 class="sg-element-column-title">Hover</h5>
+            <p><a href="#" class="link--more link--hover">Read More</a></p>
+          </div>
+          <div class="sg-element-column">
+            <h5 class="sg-element-column-title">Active</h5>
+            <p><a href="#" class="link--more link--active">Read More</a></p>
+          </div>
+        </div>
+      </article>
+      <article class="sg-item">
+        <header class="sg-item-header">
+          <h3 class="sg-item-title">General Navigation</h3>
+        </header>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Tabs</h4>
+          <ul class="nav-tabs">
+            <li class="nav--tab active"><a href="#" class="">Item 1</a></li>
+            <li class="nav--tab"><a href="#" class="">Item 2</a></li>
+            <li class="nav--tab"><a href="#" class="">Item 3</a></li>
+            <li class="nav--tab nav--tab--hover"><a href="#" class="">Item 4</a></li>
+            <li class="nav--tab nav--tab--active"><a href="#" class="">Item 5</a></li>
+          </ul>
+        </div>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Breadcrumb</h4>
+          <ul class="nav-breadcrumb">
+            <li class="nav--breadcrumb--item"><a href="#" class="">Home</a></li>
+            <li class="nav--breadcrumb--item"><a href="#" class="">Primary</a></li>
+            <li class="nav--breadcrumb--item"><a href="#" class="">Secondary</a></li>
+            <li class="nav--breadcrumb--item">Current Page</li>
+          </ul>
+        </div>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Tags</h4>
+          <div class="terms">
+            <a href="#" class="">Item 1</a> <a href="#" class="">Item 2</a> <a href="#" class="">Item 3</a>
+          </div>
+        </div>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Pager</h4>
+          <ul class="pager pager-mini">
+            <li class="pager-previous"><a href="#">Previous</a></li>
+            <li class="pager-next"><a href="#">Next</a></li>
+          </ul>
+          <ul class="pager">
+            <li class="pager-first"><a href="#">First</a></li>
+            <li class="pager-previous"><a href="#">Previous</a></li>
+            <li class="pager-item"><a href="#">1</a></li>
+            <li class="pager-item"><a href="#">2</a></li>
+            <li class="pager-current">3</li>
+            <li class="pager-item"><a href="#">4</a></li>
+            <li class="pager-item"><a href="#">5</a></li>
+            <li class="pager-ellipsis">&#8230;</li>
+            <li class="pager-next"><a href="#">Next</a></li>
+            <li class="pager-last"><a href="#">Last</a></li>
+          </ul>
+        </div>
+      </article>
+    </section>
+    <section class="sg-section">
+      <h2 class="sg-section-title">Forms</h2>
+      <article class="sg-item">
+        <header class="sg-item-header">
+          <h3 class="sg-item-title">Text Fields</h3>
+        </header>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Text Input</h4>
+          <div class="field-container">
+            <label for="text">Text Input <abbr title="Required">*</abbr></label>
+            <input id="text" type="text" placeholder="Text Input">
+          </div>
+        </div>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Password</h4>
+          <div class="field-container">
+            <label for="password">Password</label>
+            <input id="password" type="password" placeholder="Type your Password">
+          </div>
+        </div>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Web Address</h4>
+          <div class="field-container">
+            <label for="webaddress">Web Address</label>
+            <input id="webaddress" type="url" placeholder="http://yoursite.com">
+          </div>
+        </div>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Email Address</h4>
+          <div class="field-container">
+            <label for="emailaddress">Email Address</label>
+            <input id="emailaddress" type="email" placeholder="name@email.com">
+          </div>
+        </div>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Search</h4>
+          <div class="field-container">
+            <label for="search">Search</label>
+            <input id="search" type="search" placeholder="Enter Search Term">
+          </div>
+        </div>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Number Input</h4>
+          <div class="field-container">
+            <label for="text">Number Input <abbr title="Required">*</abbr></label>
+            <input id="text" type="number" placeholder="Enter a Number" pattern="[0-9]*">
+          </div>
+        </div>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Textarea</h4>
+          <div class="field-container">
+            <label for="textarea">Textarea</label>
+            <textarea id="textarea" rows="8" cols="48" placeholder="Enter your message here"></textarea>
+          </div>
+        </div>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Error Input</h4>
+          <div class="field-container">
+            <label class="error">Error Input</label>
+            <input class="is-error" type="text" placeholder="Text Input">
+          </div>
+        </div>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Valid</h4>
+          <div class="field-container">
+            <label class="valid">Valid</label>
+            <input class="is-valid" type="text" placeholder="Text Input">
+          </div>
+        </div>
+      </article>
+      <article class="sg-item">
+        <header class="sg-item-header">
+          <h3 class="sg-item-title">Select Menu</h3>
+        </header>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Select</h4>
+          <div class="field-container">
+            <label for="select">Select</label>
+            <select id="select">
+              <optgroup label="Option Group">
+                <option>Option One</option>
+                <option>Option Two</option>
+                <option>Option Three</option>
+              </optgroup>
+            </select>
+          </div>
+        </div>
+      </article>
+      <article class="sg-item">
+        <header class="sg-item-header">
+          <h3 class="sg-item-title">Options</h3>
+        </header>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Checkboxes</h4>
+          <div class="field-container options">
+            <legend>Checkbox <abbr title="Required">*</abbr></legend>
+            <ul>
+              <li><label for="checkbox1"><input id="checkbox1" name="checkbox" type="checkbox" checked="checked"> Choice A</label></li>
+              <li><label for="checkbox2"><input id="checkbox2" name="checkbox" type="checkbox"> Choice B</label></li>
+              <li><label for="checkbox3"><input id="checkbox3" name="checkbox" type="checkbox"> Choice C</label></li>
+            </ul>
+          </div>
+        </div>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Radio Buttons</h4>
+          <div class="field-container options">
+            <legend>Radio</legend>
+            <ul>
+              <li><label for="radio1"><input id="radio1" name="radio" type="radio" class="radio" checked="checked"> Option 1</label></li>
+              <li><label for="radio2"><input id="radio2" name="radio" type="radio" class="radio"> Option 2</label></li>
+              <li><label for="radio3"><input id="radio3" name="radio" type="radio" class="radio"> Option 3</label></li>
+            </ul>
+          </div>
+        </div>
+      </article>
+    </section>
+    <section class="sg-section">
+      <h2 class="sg-section-title">Tables</h2>
+      <article class="sg-item">
+        <header class="sg-item-header">
+          <h3 class="sg-item-title">Basic Table</h3>
+        </header>
+        <div class="sg-element">
+          <h4 class="sg-element-title">Table</h4>
+          <table>
+            <thead>
+              <tr>
+                <th>Table Heading 1</th>
+                <th>Table Heading 2</th>
+                <th>Table Heading 3</th>
+                <th>Table Heading 4</th>
+              </tr>
+            </thead>
+            <tfoot>
+              <tr>
+                <th>Table Footer 1</th>
+                <th>Table Footer 2</th>
+                <th>Table Footer 3</th>
+                <th>Table Footer 4</th>
+              </tr>
+            </tfoot>
+            <tbody>
+              <tr class="odd">
+                <td>Table Cell 1</td>
+                <td>Table Cell 2</td>
+                <td>Table Cell 3</td>
+                <td>Table Cell 4</td>
+              </tr>
+              <tr class="even">
+                <td>Table Cell 1</td>
+                <td>Table Cell 2</td>
+                <td>Table Cell 3</td>
+                <td>Table Cell 4</td>
+              </tr>
+              <tr class="odd">
+                <td>Table Cell 1</td>
+                <td>Table Cell 2</td>
+                <td>Table Cell 3</td>
+                <td>Table Cell 4</td>
+              </tr>
+              <tr class="even">
+                <td>Table Cell 1</td>
+                <td>Table Cell 2</td>
+                <td>Table Cell 3</td>
+                <td>Table Cell 4</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </article>
     </section>
