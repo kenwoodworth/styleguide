974bf66756b61ae1e25942a499691447791d53d6
Ken Woodworth <ken@atendesigngroup.com>
2014-06-24 11:51:10 -0400

    initial commit of styleguide files

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
+<head>
+  <title>StyleGuide</title>
+  <script type="text/javascript" src="//use.typekit.net/mfg3jbg.js"></script>
+  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+  <link rel="stylesheet" href="../css/screen.css">
+  <link rel="stylesheet" href="../css/styleguide.css">
+</head>
+
+<body class="styleguide">
+  <div class="l--constrained">
+    <h1 class="styleguide">Style Guide</h1>
+    <section class="styleguide">
+      <h2 class="styleguide">Colors</h2>
+      <article class="styleguide">
+        <header class="styleguide">
+          <h3 class="styleguide">Primary</h3>
+        </header>
+        <div class="styleguide-color" style="background:#e5efeb;"><strong>$teal-white</strong><span>#e5efeb</span></div>
+        <div class="styleguide-color" style="background:#dae8e2;"><strong>$teal-lightest</strong><span>#dae8e2</span></div>
+        <div class="styleguide-color" style="background:#3dd59e;"><strong>$teal-lighter</strong><span>#3dd59e</span></div>
+        <div class="styleguide-color" style="background:#00c584;"><strong>$teal-light</strong><span>#00c584</span></div>
+        <div class="styleguide-color styleguide-color-main" style="background:#01986c;"><strong>$teal</strong><span>#01986c</span></div>
+        <div class="styleguide-color" style="background:#017864;"><strong>$teal-dark</strong><span>#017864</span></div>
+        <div class="styleguide-color" style="background:#00585c;"><strong>$teal-darker</strong><span>#00585c</span></div>
+        <div class="styleguide-color" style="background:#001d20;"><strong>$teal-darkest</strong><span>#001d20</span></div>
+      </article>
+      <article class="styleguide">
+        <header class="styleguide">
+          <h3 class="styleguide">Secondary</h3>
+        </header>
+        <div class="styleguide-color styleguide-color-main" style="background:#50625c;"><strong>$gray-green</strong><span>#50625c</span></div>
+      </article>
+    </section>
+    <section class="styleguide">
+      <h2 class="styleguide">Typography</h2>
+      <article class="styleguide">
+        <header class="styleguide">
+          <h3 class="styleguide">Headings</h3>
+        </header>
+        <h1>Heading 1 with a <a href="#">link</a></h1>
+        <h2>Heading 2 with a <a href="#">link</a></h2>
+        <h3>Heading 3 with a <a href="#">link</a></h3>
+        <h4>Heading 4 with a <a href="#">link</a></h4>
+        <h5>Heading 5 with a <a href="#">link</a></h5>
+        <h6>Heading 6 with a <a href="#">link</a></h6>
+      </article>
+    </section>
+  </div>
+</body>
+</html>
